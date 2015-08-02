12044cdb2302aca1fb2e1b0a6bc433df13495646
Roman <roman@written.ru>
2015-08-02 11:49:10 +0300

    Editor script refactoring.

--- www/js/init_editor.js
+++ www/js/init_editor.js
@@ -1,99 +1,137 @@
+var Renderer = (function () {
+	var source,
+		format,
+		callback = function () {},
+		timeout;
+
+	function timerTick() {
+		callback(source, format);
+	}
+
+	function update() {
+		if (timeout) {
+			clearTimeout(timeout);
+		}
+		timeout = setTimeout(timerTick, 300);
+	}
+
+	return {
+		setFormat: function (value) {
+			if (format !== value) {
+				format = value;
+				update();
+			}
+		},
+		setSource: function (value) {
+			if (source !== value) {
+				source = value;
+				update();
+			}
+		},
+		getSource: function () {
+			return source;
+		},
+		setCallback: function (f) {
+			callback = f;
+		},
+		resetTimer: function () {
+
+		}
+	};
+}());
+
 function initTexEditor (serviceURL)
 {
 	var $source = $('.editor-text'),
-		text,
 		preview = document.getElementById('editor-preview'),
-		timeout,
 		old_url;
 
-	function timer ()
+	function timerTick (text, format)
 	{
-		var
-			text = $.trim($source.val()),
-			format = document.getElementById('svg_radio').checked ? 'svg' : 'png',
-			url = text ? serviceURL + format + '/' + encodeURIComponent($.trim(text)) : '';
+		var url = text ? serviceURL + format + '/' + encodeURIComponent(text) : '';
 
-		if (url === old_url)
-			return;
+		if (url !== old_url) {
+			old_url = url;
 
-		old_url = url;
-
-		document.forms['editor'].result.value = url;
-		preview.src = url;
+			document.forms['editor'].result.value = url;
+			preview.src = url;
+		}
 	}
 
-	$(document.forms['editor'].elements['format']).on('change', timer);
+	Renderer.setCallback(timerTick);
 
-	function setupTimer()
-	{
-		var value = $.trim($source.val());
-		if (value === text)
-			return;
-
-		text = value;
-
-		if (timeout)
-			clearTimeout(timeout);
-
-		timeout = setTimeout(timer, 300);
+	function updateFormat() {
+		Renderer.setFormat(document.getElementById('svg_radio').checked ? 'svg' : 'png');
 	}
 
-	$source.on('propertychange keyup input paste', setupTimer);
+	$(document.forms['editor'].elements['format']).on('change', updateFormat);
+	updateFormat();
 
+	// Waits for 300 ms when the formula is changed and renders it.
+	function updateSource()
+	{
+		Renderer.setSource($.trim($source.val()));
+	}
+
+	updateSource();
+	$source.on('propertychange keyup input paste', updateSource);
+
+	// Select the content of URL field on focus.
 	$('.editor-result').on('focus', function () {
 		var that = this;
-		setTimeout(function ()
-		{
+		setTimeout(function () {
 			that.select();
 		}, 10);
 	});
 
 	autosize($source);
 
-	$('#editor-preview').error(function () {
-		if (text !== '')
+	// Highlight textarea when the formula is invalid.
+	$(preview).error(function () {
+		if (Renderer.getSource() !== '') {
 			setTimeout(function () { $source.removeClass('load-error').addClass('load-error'); }, 0);
+		}
 	}).load(function () {
 		$source.removeClass('load-error');
 	});
 
-	$('.add-formula').click(function ()
-	{
-		var cur = $source.val();
+	// Add sample formula to the textarea.
+	$('.add-formula').click(function () {
+		var cur = $source.val(),
+			sampleText = $(this).parent().children('.sample-source').text();
+
 		if (cur)
 			cur += "\n";
-		$source.val(cur + $(this).parent().children('.sample-source').text()).trigger('autosize.resize');
-		setupTimer();
+		$source.val(cur + sampleText).trigger('autosize.resize');
+		updateSource();
 		scrollPage($('#editor'));
 	});
 
-	setupTimer();
-
-	function scrollPage ($target)
-	{
+	function scrollPage ($target) {
 		$('html,body').animate({
 		  scrollTop: $target.offset().top - 45
 		}, 300);
 	}
 
-	$('a.inside').click(function()
-	{
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname)
-		{
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	$('a.inside').click(function() {
+		if (location.pathname.replace(/^\//,'') !== this.pathname.replace(/^\//,'')) {
+			return;
+		}
+		if (location.hostname !== this.hostname) {
+			return;
+		}
 
-			if (target.length)
-			{
-				scrollPage(target);
-				return false;
-			}
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+
+		if (target.length) {
+			scrollPage(target);
+			return false;
 		}
 	});
 }
 
-function initTexSite ()
-{
+function initTexSite() {
 	$('.sticky').Stickyfill();
 	new Image().src = "//counter.yadro.ru/hit?r"+escape(document.referrer)+((typeof(screen)=="undefined")?"":";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+";h"+escape(document.title.substring(0,80))+";"+Math.random();
 }
