92ee6753b36bcca3eed55fc83fc39d8ff80ab396
Roman Parpalak <roman@written.ru>
2017-11-06 18:08:33 +0300

    Redesigned header.

--- public_html/index.php
+++ public_html/index.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright (C) 2015-2016 Roman Parpalak
+ * @copyright (C) 2015-2017 Roman Parpalak
  * @license   http://www.opensource.org/licenses/mit-license.php MIT
  * @package   Markdown & LaTeX Editor
  * @link      https://upmath.me/
@@ -21,10 +21,10 @@ $formats = [
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Markdown & LaTeX Online Editor</title>
+	<title>Upmath: Markdown & LaTeX Online Editor</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="keywords" content="LaTeX, Markdown, equations, markdown latex online editor, tikz, latex online">
-	<meta name="description" content="Markdown & LaTeX Online Editor helps writing math texts for the web. It converts Markdown syntax to HTML, and LaTeX equations to SVG images.">
+	<meta name="description" content="Upmath helps writing math texts for the web. It converts Markdown syntax to HTML, and LaTeX equations to SVG images.">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" type="image/png" href="/favicon.png">
 
@@ -35,73 +35,65 @@ $formats = [
 	<link rel="stylesheet" href="/src/css/editor.css">
 	<link rel="stylesheet" href="/lib/highlight.js/solarized-light.css">
 <?php endif; ?>
+
 	<link rel="dns-prefetch" href="//tex.s2cms.ru">
 	<link rel="preconnect" href="//tex.s2cms.ru" crossorigin>
 	<link rel="prefetch" href="/i/latex.jpg">
 </head>
+
 <body>
-<div class="header">
-	<h1 class="header-item title">Markdown & LaTeX Editor</h1>
-	<div class="header-item copyright">
-		&copy; 2016
-		<script>
-			var mailto = "roman%"+"40written.ru";
-			document.write('<a class="link" title="Drop me a line" href="mailto:'+unescape(mailto)+'">Roman Parpalak</a>');
-		</script>
-	</div>
-</div>
-<div class="container full-height" id="container-block">
-	<div class="half-width full-height" id="source-block">
-		<div class="toolbar left">
+	<div class="header">
+		<h1 class="header-item title">Markdown & LaTeX Editor</h1>
+		<div class="header-item open-control">
 			<input type="file" id="fileElem" style="display:none">
-			<button class="toolbar-button _upload-source" title="Upload">⇑</button>
+			<button class="toolbar-button _upload-source" title="Upload a file">⇑</button>
+			<button class="toolbar-button _download-source" title="Download source">⇓</button>
 		</div>
-		<div class="toolbar right">
-			<button class="toolbar-button _download-source" title="Download">⇓</button>
+		<div class="header-item demo-control">
+			<?php
+
+			$selected = true;
+
+			foreach ($formats as $class => $name) {
+				?><input
+						class="control-input"
+						id="id_<?php echo $class; ?>"
+						type="radio"
+						name="source_type"
+					<?php $selected ? print 'checked="checked"' : null; ?>
+				><label for="id_<?php echo $class; ?>" data-result-as="<?php echo $class; ?>" class="control-item"><?php echo $name; ?></label><?php
+				$selected = false;
+			}
+			?>
+			<button class="toolbar-button _download-result" title="Download result">⇓</button>
+		</div>
+		<div class="header-item copyright">
+			&copy; 2015–2017<br>
+			<script>
+				var mailto = "roman%"+"40written.ru";
+				document.write('<a class="link" title="Drop me a line" href="mailto:'+unescape(mailto)+'">Roman Parpalak</a>');
+			</script>
+		</div>
+	</div>
+	<div class="container full-height" id="container-block"><!--
+		--><div class="half-width full-height source-wrap" id="source-block"><textarea class="source full-height"></textarea></div><!--
+		--><div class="half-width full-height" id="result-block">
+			<div class="result-html full-height"></div>
+			<pre class="result-src full-height"><code class="result-src-content"></code></pre>
+			<pre class="result-htmltex full-height"><code class="result-htmltex-content"></code></pre>
+			<pre class="result-habr full-height"><code class="result-habr-content"></code></pre>
+			<pre class="result-debug full-height"><code class="result-debug-content"></code></pre>
 		</div><!--
-		--><textarea class="source full-height"></textarea><!--
-	--></div><!--
-	--><div class="half-width full-height" id="result-block">
-		<div class="toolbar right">
-			<button class="toolbar-button _download-result" title="Download">⇓</button>
-		</div>
-		<div class="demo-control">
-<?php
-
-$selected = true;
-
-foreach ($formats as $class => $name)
-{
-?>
-			<input
-				class="control-input"
-				id="id_<?php echo $class; ?>"
-				type="radio"
-				name="source_type"
-				<?php $selected ? print 'checked="checked"' : null; ?>
-			>
-			<label for="id_<?php echo $class; ?>" data-result-as="<?php echo $class; ?>" class="control-item"><?php echo $name; ?></label>
-<?php
-	$selected = false;
-}
-?>
-		</div>
-		<div class="result-html full-height"></div>
-		<pre class="result-src full-height"><code class="result-src-content full-height"></code></pre>
-		<pre class="result-htmltex full-height"><code class="result-htmltex-content full-height"></code></pre>
-		<pre class="result-habr full-height"><code class="result-habr-content full-height"></code></pre>
-		<pre class="result-debug full-height"><code class="result-debug-content full-height"></code></pre>
-	</div><!--
---></div>
-<script>
-	(function () {
-		try {
-			var data = localStorage.getItem("editor_content");
-		}
-		catch (e) {}
-		document.getElementsByClassName('source')[0].value = data || <?php echo json_encode(file_get_contents('sample.md'), JSON_UNESCAPED_UNICODE); ?>;
-	}());
-</script>
+	--></div>
+	<script>
+		(function () {
+			try {
+				var data = localStorage.getItem("editor_content");
+			}
+			catch (e) {}
+			document.getElementsByClassName('source')[0].value = data || <?php echo json_encode(file_get_contents('sample.md'), JSON_UNESCAPED_UNICODE); ?>;
+		}());
+	</script>
 <?php if (defined('USE_MINIFICATION') && USE_MINIFICATION): ?>
 	<script src="/dist/js/vendors.min.js?<?php include 'dist/js/vendors.min.js.md5'; ?>"></script>
 	<script src="/dist/js/scripts.min.js?<?php include 'dist/js/scripts.min.js.md5'; ?>"></script>
