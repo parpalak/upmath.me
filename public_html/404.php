f72209d995ca3450ab7ab76c7ff0523c52ef3204
Roman Parpalak <roman@written.ru>
2020-09-05 13:40:20 +0300

    Implemented 404 page.

---
+++ public_html/404.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Upmath: Markdown & LaTeX Online Editor</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="LaTeX, Markdown, equations, markdown latex online editor, tikz, latex online">
+    <meta name="description" content="Upmath helps writing math texts for the web. It converts Markdown syntax to HTML, and LaTeX equations to SVG images.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="preconnect" href="//fonts.gstatic.com">
+    <link rel="shortcut icon" type="image/png" href="/favicon.png">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i&amp;subset=cyrillic,greek" rel="stylesheet" media="none" onload="if(media!='all')media='all'">
+
+<?php if (defined('USE_MINIFICATION') && USE_MINIFICATION): ?>
+    <link rel="stylesheet" href="/dist/css/style.min.css?<?php include 'dist/css/style.min.css.md5'; ?>">
+<?php else: ?>
+    <link rel="stylesheet" href="/dist/css/TextareaDecorator.css">
+    <link rel="stylesheet" href="/src/css/editor.css">
+    <link rel="stylesheet" href="/lib/highlight.js/solarized-light.css">
+<?php endif; ?>
+
+    <link rel="preconnect" href="//i.upmath.me" crossorigin>
+    <link rel="prefetch" href="/i/latex.jpg" as=image>
+</head>
+<body class="e404">
+    <div class="e404-header">
+		<a href="/"><h1 class="title">Markdown & LaTeX Editor</h1></a>
+    </div>
+	<div class="e404-body">
+		<p class="e404-text">404</p>
+		<p>Page not found.</p>
+		<p><a href="/" style="color: currentColor;">Go to the main page</a> and write a cool math text.</p>
+	</div>
+</body>
+</html>
