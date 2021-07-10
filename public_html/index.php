de2a0bafaef9ee731a6db987e6d6c5a8dc7c5a3a
Roman Parpalak <roman@written.ru>
2021-07-10 23:32:58 +0300

    Used JetBrains Mono as a fallback font for MacOS due to char width variations for bold Menlo font version (reported by 3x).

--- public_html/index.php
+++ public_html/index.php
@@ -30,9 +30,9 @@ $formats = [
 	<meta name="keywords" content="LaTeX, Markdown, equations, markdown latex online editor, tikz, latex online">
 	<meta name="description" content="Upmath helps writing math texts for the web. It converts Markdown syntax to HTML, and LaTeX equations to SVG images.">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="preconnect" href="//fonts.gstatic.com">
+	<link rel="preconnect" href="//fonts.gstatic.com" crossorigin>
 	<link rel="shortcut icon" type="image/png" href="/favicon.png">
-	<link href="https://fonts.googleapis.com/css?family=Noto+Serif:400,400i,700,700i&amp;subset=cyrillic,greek" rel="stylesheet" media="none" onload="if(media!='all')media='all'">
+	<link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;700&family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700" rel="stylesheet" media="none" onload="if(media!='all')media='all'">
 
 <?php if (defined('USE_MINIFICATION') && USE_MINIFICATION): ?>
 	<link rel="stylesheet" href="/dist/css/style.min.css?<?php include 'dist/css/style.min.css.md5'; ?>">
